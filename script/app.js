57fe29a8d861e462e27e9000300b209cddf6308e
payne-chris-r <06cpayne@gmail.com>
2015-07-02 16:41:40 -0400

    Added app.js and made changes to the .css file

---
+++ script/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//I think this will be handled by a 'click' event handler, but if I was going to use a prompt/entry field I would do it this way.
+var getInput = function() {
+  var input = '';
+
+  return input;
+};
+
+//This function should change the value of the inner html from the square number to X or O depending on the player (maybe using setPlayer()??)
+var changeValue = function() {
+  var newValue = '';
+  return newValue;
+};
+
+//This function should run to determine which player's turn it is, and maybe when setting the square to a certain value
+var setPlayer = function() {
+  var player = '';
+  return player;
+};
+
+//this function needs to run after a square has been played in to make sure no one else can play in that square again.
+var setUneditable = function() {
+  var square = '';
+  return square;
+};
+
+var setInput
+
+//this function will decide when the game is over, and should run after each player takes his/her turn.
+var declareWinner = function (){
+  var winner = '';
+
+  return winner;
+};
+
