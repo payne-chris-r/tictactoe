cff493440c644953b15509c4a3f1cbcab4d5d496
payne-chris-r <06cpayne@gmail.com>
2015-07-06 22:55:53 -0400

    JQ is now working. Added some logic to change the clicked '.square' to fade when clicked as well as replace the html with 'X'.

--- script/jq.js
+++ script/jq.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
   alert("hell!");
 });
 
+
+$('.square').click(function(){
+    $(this).fadeTo('slow', 0.5);
+    $(this).html('X');
+});
+
+
+
 // $('square').ready(function(){
 
 // };
