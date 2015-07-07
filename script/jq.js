2e15e2e65f36616d1b076dcd8d12926b84fec406
payne-chris-r <06cpayne@gmail.com>
2015-07-06 22:08:48 -0400

    Ohhh shit! Check me out! Finally got my js linked and pulling in the jquery library.

---
+++ script/jq.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready(function(){
+  alert("hell!");
+});
+
+// $('square').ready(function(){
+
+// };
+
+// $('#square').click(function(){
+//   $(this).fadeTo('slow', 0.5);
+// });
+
